6fb25c19c5affbe3f2d433f41741b7492b241f51
Alfred Tuva <muindetuva@gmail.com>
2023-06-30 16:40:24 +0300

    Add missing flask things

---
+++ flask_api/app/models.py
@@ -0,0 +1,39 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(120))
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'name': self.name,
+            'email': self.email
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['username', 'name', 'email', 'password']:
+            if field in data:
+                setattr(self, field, data[field])
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    content = db.Column(db.String(140))
+    cover_image = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
--- flask_api/app/routes.py
+++ flask_api/app/routes.py
@@ -1,6 +1,44 @@
-from app import app
+from app import app, db
+from app.models import User
+from flask import jsonify, request
 
 
 @app.route('/')
 def index():
     return "Hello World"
+
+
+@app.route('/api/users')
+def all_users():
+
+    users = User.query.all()
+
+    users_data = [user.to_dict() for user in users]
+
+    return jsonify(users_data)
+
+@app.route('/api/users/<int:id>')
+def user(id):
+
+    user = User.query.get(id)
+
+    return jsonify(user.to_dict())
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+
+    # GEt data from request
+    data = request.get_json()
+    # store data in db
+    user = User()
+
+    user.from_dict(data)
+
+    db.session.add(user)
+    db.session.commit()
+
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+
+    return response
+
