205bbd02800ac25f041ed8ff7a28cbfbe7412cd8
Alfred Tuva <muindetuva@gmail.com>
2023-07-05 17:14:17 +0300

    lates files

--- flask_api/app/models.py
+++ flask_api/app/models.py
@@ -37,3 +37,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.title}>"
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+
